4f508ebea89eb7f6285a9dd11ddcdb64e48f6bff
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-12-02 17:05:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,9 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,41 @@
+package br.com.alura.orgs.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val nome = findViewById<TextView>(R.id.nome)
+//        nome.text = "Cesta de frutas"
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, manga e maçã"
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+                Produto(nome = "teste",
+                        descricao = "teste desc",
+                        valor = BigDecimal("19.99")
+                ),
+                Produto(nome = "teste 1",
+                        descricao = "teste desc 1",
+                        valor = BigDecimal("29.99")
+                ),
+                Produto(nome = "teste 2",
+                        descricao = "teste desc 2",
+                        valor = BigDecimal("39.99")
+                ),
+        ))
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+}
