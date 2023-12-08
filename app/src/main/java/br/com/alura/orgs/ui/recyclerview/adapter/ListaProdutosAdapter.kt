ea27775abd79197b28960b0328d28a560367aa5e
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-12-08 16:25:45 -0300

    Finalizando primeiro curso de Apps android

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -11,17 +11,19 @@ import br.com.alura.orgs.model.Produto
 
 class ListaProdutosAdapter(
         private val context: Context,
-        private val produtos: List<Produto>
+        produtos: List<Produto>
 ) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    private val produtos = produtos.toMutableList()
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         fun vincula(produto: Produto) {
-            val nome = itemView.findViewById<TextView>(R.id.nome)
+            val nome = itemView.findViewById<TextView>(R.id.produto_item_nome)
             nome.text = produto.nome
-            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            val descricao = itemView.findViewById<TextView>(R.id.produto_item_descricao)
             descricao.text = produto.descricao
-            val valor = itemView.findViewById<TextView>(R.id.valor)
+            val valor = itemView.findViewById<TextView>(R.id.produto_item_valor)
             valor.text = produto.valor.toPlainString()
         }
 
@@ -39,5 +41,11 @@ class ListaProdutosAdapter(
     }
 
     override fun getItemCount(): Int = produtos.size
+
+    fun atualiza(produtos: List<Produto>) {
+        this.produtos.clear()
+        this.produtos.addAll(produtos)
+        notifyDataSetChanged()
+    }
 
 }
